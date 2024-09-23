66eea72eb5d7cf8ea694cff2adeba2a426662174
vwh <vwhe@proton.me>
2024-09-24 02:34:18 +0300

    build: update tsconfig and tsup

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   format: ["cjs", "esm"],
   entry: ["src/index.ts", "src/bun.ts"],
+  minify: true,
   dts: true,
   shims: true,
   skipNodeModulesBundle: true,
